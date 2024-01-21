06ad88620470e57a5b81cde9fc12a3df826845ff
David Brain <116792506+ShadyIsCool@users.noreply.github.com>
2024-01-21 21:13:12 +0000

    Delete SetBlackBackground.js

--- SetBlackBackground.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the body element
-    var body = document.body;
-
-    // Set the background color to black
-    body.style.backgroundColor = "black";
-});
