6d0b6dc391fa31cd180c161affdd0f775f161877
David Brain <116792506+ShadyIsCool@users.noreply.github.com>
2024-01-21 20:24:03 +0000

    Add files via upload

---
+++ SetBlackBackground.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the body element
+    var body = document.body;
+
+    // Set the background color to black
+    body.style.backgroundColor = "black";
+});
